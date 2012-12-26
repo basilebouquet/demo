d33dd1db3d412feb3512d25bec5fe3966c4d4ba2
basile bouquet <bas.bouquet@gmail.com>
2012-12-26 08:43:41 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '576df218fcf2971ac587d7d5fb5a95f6937d3b4bf9abb5166c606235724433b282b1b431d7be83193790b13f520ada5c611e5c5670a8ff2dc3a49e8902e02fd0'
